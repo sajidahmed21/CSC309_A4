81e2bdb53c3bba81677d8ee62c14fffd0c84d9fb
sk-csc309 <sajjad.forums@gmail.com>
2016-07-09 18:35:31 -0400

    Implemented resizing of the messaging area.

--- assets/js/messages.js
+++ assets/js/messages.js
@@ -24,8 +24,30 @@ function showChatArea(partnerId) {
 }
 
 
+/* Resizes the main messaging area so that it uses the entire available height */
+function resizeMessagingArea() {
+    // calculate the used height
+    var $elements = $('body > *:not(main#messaging)');
+    
+    var usedHeight = 0;
+    $elements.each(function(index, element) {
+        // include the margin in the calculation
+        usedHeight += $(element).outerHeight(true);
+    });
+    
+    // set the height of the messaging area
+    $('main#messaging').height($(window).height() - usedHeight);
+    
+    return;
+}
+
+
 window.onload = function() {
     // load the first chat area (which for the mockup is hiding all
     // chat areas except the first
     showChatArea(41);
+    
+    // set the resizing of the messaging area and trigger it immediately
+    $(window).resize(resizeMessagingArea);
+    resizeMessagingArea();
 }
