f4380d191f6ed99aee24e0d9389fa47a3dc4bb59
Willis Chien <willis.chien@mail.utoronto.ca>
2016-07-11 11:19:50 -0400

    fix header and popups

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,184 @@
 $( document ).ready(function() {
     initialize();
     $('#signup_btn').click(function(){
-        var $signup_form = $('#signup_form');
-        $signup_form.css('display', 'block');
+        signupPage();
     });
     $('#userlogin_btn').click(function(){
-        var $signup_form = $('#userlogin_form');
-        $signup_form.css('display', 'block');
-    });
-    $('#adminlogin_btn').click(function(){
-        var $signup_form = $('#adminlogin_form');
-        $signup_form.css('display', 'block');
-    });
-    $('.cancel_icon').click(function(){
-        hidePopup();
+        loginPage();
     });
 });
 
+var loginPage = function(){
+    var $popup = $('#popup');
+    $popup.empty();
+    
+    $outer = $('<div/>', {
+       id:  'userlogin_form'
+    });
+    
+    $middle = $('<div/>', {
+       class:  'col-md-6'
+    });
+    
+    $container = $('<div/>', {
+       class:  'beautiful_container'
+    });
+    
+    $cancelwrapper = $('<div/>');
+    
+    $cancelicon = $('<span/>', {
+       class:  'glyphicon glyphicon-remove cancel_icon'
+    });
+    $cancelicon.click(function(){
+        hidePopup();
+    });
+    $cancelwrapper.append($cancelicon);
+    
+    $container.append($cancelwrapper);
+    
+    $form = $('<form/>');
+    
+    $title = $('<h1/>',{
+        class : 'beautiful_title'
+    });
+    $title.text('Login').appendTo($form);
+    
+    $input = $('<input/>',{
+        name : 'userName',
+        type : 'text',
+        placeholder : 'Username',
+        required : 'required',
+        class : 'beautiful_red_input'
+    });
+    
+    $input.appendTo($form);
+    
+    $input = $('<input/>',{
+        name : 'userPassword',
+        type : 'password',
+        placeholder : 'Password',
+        required : 'required',
+        class : 'beautiful_red_input'
+    });
+    
+    $input.appendTo($form);
+    
+    $input = $('<input/>',{
+        type : 'submit',
+        value : 'Login',
+        class : 'btn btn-primary beautiful_red_btn block_btn'
+    });
+    
+    $input.appendTo($form);
+    
+    $form.appendTo($container);
+    $container.appendTo($middle);
+    $middle.appendTo($outer);
+    $outer.appendTo($popup);
+}
+
+var signupPage = function(){
+   var $popup = $('#popup');
+    $popup.empty();
+    
+    $outer = $('<div/>', {
+       id:  'signup_form'
+    });
+    
+    $middle = $('<div/>', {
+       class:  'col-md-6'
+    });
+    
+    $container = $('<div/>', {
+       class:  'beautiful_container'
+    });
+    
+    $cancelwrapper = $('<div/>');
+    
+    $cancelicon = $('<span/>', {
+       class:  'glyphicon glyphicon-remove cancel_icon'
+    });
+    $cancelicon.click(function(){
+        hidePopup();
+    });
+    $cancelwrapper.append($cancelicon);
+    
+    $container.append($cancelwrapper);
+    
+    $form = $('<form/>');
+    
+    $title = $('<h1/>',{
+        class : 'beautiful_title'
+    });
+    $title.text('Sign Up').appendTo($form);
+    
+    $input = $('<input/>',{
+        name : 'userName',
+        type : 'text',
+        placeholder : 'Username',
+        required : 'required',
+        class : 'beautiful_input'
+    });
+    
+    $input.appendTo($form);
+    
+    $input = $('<input/>',{
+        name : 'userPassword',
+        type : 'password',
+        placeholder : 'Password',
+        required : 'required',
+        class : 'beautiful_input'
+    });
+    
+    $input.appendTo($form);
+    
+    $input = $('<input/>',{
+        name : 'userPasswordConfirm',
+        type : 'password',
+        placeholder : 'Password Comfirmation',
+        required : 'required',
+        class : 'beautiful_input'
+    });
+    
+    $input.appendTo($form);
+    
+    $input = $('<input/>',{
+        name : 'userEmail',
+        type : 'email',
+        placeholder : 'Email',
+        required : 'required',
+        class : 'beautiful_input'
+    });
+    
+    $input.appendTo($form);
+    
+    $input = $('<input/>',{
+        type : 'submit',
+        value : 'Join Now!',
+        class : 'btn btn-primary beautiful_btn block_btn'
+    });
+    
+    $input.appendTo($form);
+    
+    $form.appendTo($container);
+    $container.appendTo($middle);
+    $middle.appendTo($outer);
+    $outer.appendTo($popup);
+};
+
+
 var initialize = function(){
     hidePopup();
-}
+};
 var hidePopup = function(){
+    console.log("hide");
+    var $popup = $('#popup');
+    $popup.empty();
+    
     var $signup_form = $('#signup_form');
     var $userlogin_form = $('#userlogin_form');
     var $adminlogin_form = $('#adminlogin_form');
     $signup_form.css('display', 'none');
     $userlogin_form.css('display', 'none');
     $adminlogin_form.css('display', 'none');
-}
+};
