ccc2e9ac120876cf0d4be27b5a968d0ca7951d22
sk-csc309 <sajjad.forums@gmail.com>
2016-07-19 22:27:08 -0400

    Added some dummy pages to play with.

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var app = express();
 
+// handlebars setup
+var exphbs  = require('express-handlebars');
+var hbs = exphbs.create({
+	defaultLayout: false
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// set public directory for css, js, and imgs
 app.use(express.static('public'));
 
 
 /* page routing -----------------------------------------------------*/
 app.get('/', function (req, res) {
-  
+	res.render('home');
+});
+
+
+app.get('/demo', function (req, res) {
+	res.render('demo', {
+		demo: true
+	});
 });
 
 
