0600a4efcc2429679dac9134e66ac864c1dc3ada
sk-csc309 <sajjad.forums@gmail.com>
2016-07-21 00:56:48 -0400

    Added in code for recommendations to work properly.

--- recommendations.js
+++ recommendations.js
@@ -5,9 +5,41 @@ var sendBackJSON = common.sendBackJSON;
 
 exports.userCourses = function(req, res) {
     // todo: get userId from session
-    var userId = req.body.user_id;
+    if (req.query.user_id > 0) {
+        var queryString =
+            'SELECT E.class_id, count(*) as user_count, ' +
+            'C.class_name ' +
+            'FROM ENROLMENT E ' +
+            'INNER JOIN Classes C ' +
+                'ON C.id = E.class_id ' +
+            'WHERE E.user_id IN ( ' +
+            	'SELECT DISTINCT E2.user_id ' +
+            	'FROM ENROLMENT E2 ' +
+            	'WHERE E2.class_id in ( ' +
+            		'SELECT DISTINCT E1.class_id ' +
+            		'FROM ENROLMENT E1 ' +
+            		'WHERE E1.user_id = ? ' +
+            	') ' +
+            ') ' +
+            'GROUP BY E.class_id ' +
+            'ORDER BY user_count DESC ' +
+            'LIMIT 10 '
+        ;
     
-    db.query('select count(*) as userCount from users').spread(function(result, metadata) {
-        sendBackJSON({"count": result[0].userCount}, res);
-    });
+        db.query(queryString, {
+            replacements: [req.query.user_id]
+        })
+        .spread(function(result, metadata) {
+            sendBackJSON({"courses": result}, res);
+        })
+        .catch(function(err) {
+            console.log('failed to fetch recommendations:');
+            console.log(err);
+            
+            sendBackJSON({'error': 'server error'}, res);
+        });
+    }
+    else {
+        sendBackJSON({'error': 'no user is logged in'}, res);
+    }
 }
