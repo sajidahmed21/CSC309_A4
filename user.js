9246893b44b94d8174ffcf2044379d4363882c7e
Willis Chien <willis.chien@mail.utoronto.ca>
2016-07-21 14:51:10 -0400

    fixed bug in user

--- user.js
+++ user.js
@@ -42,93 +42,108 @@ app.get('/enroll', function (req, res) {
     });
 });*/
 var common = require('./common');
+var bcrypt = require('bcryptjs');
 var sendBackJSON = common.sendBackJSON;
 var db = common.db;
 
 exports.changeNameHandler = function (req, res) {
     var changeName = req.body.changeName;
     var user_id = req.body.user_id;
-    db.query("UPDATE USERS SET name = '"+changeName+"' WHERE id=" + user_id).spread(function(results, metadata){
+    db.query("UPDATE USERS SET name = '" + changeName + "' WHERE id=" + user_id).spread(function (results, metadata) {
         var returnJSON = {
-            "success": "Change Name Success"
+            "status": "success",
+            "message": "Change Name Success"
         }
         sendBackJSON(returnJSON, res);
-    }).catch(function(err){
+    }).catch(function (err) {
         console.log("Err in change name");
         var returnJSON = {
-            "error": "Err in change name"
+            "status": "error",
+            "message": "Err in change name"
         }
         sendBackJSON(returnJSON, res);
     });
 };
-exports.changeProfilePicHandler = function(req, res){
+exports.changeProfilePicHandler = function (req, res) {
     var changeProfilepic = req.body.changeProfilepic;
     var user_id = req.body.user_id;
-    db.query("UPDATE USERS SET profile_picture_path = '"+changeProfilepic+"' WHERE id=" + user_id).spread(function(results, metadata){
+    db.query("UPDATE USERS SET profile_picture_path = '" + changeProfilepic + "' WHERE id=" + user_id).spread(function (results, metadata) {
         var returnJSON = {
-            "success": "Change Profile Pic Success"
+            "status": "success",
+            "message": "Change Profile Pic Success"
         }
         sendBackJSON(returnJSON, res);
-    }).catch(function(err){
+    }).catch(function (err) {
         console.log("Err in change profile pic");
         var returnJSON = {
-            "error": "Err in change profile pic"
+            "status": "error",
+            "message": "Err in change profile pic"
         }
         sendBackJSON(returnJSON, res);
     });
 };
-exports.changePasswordHandler = function (req, res){
-    bcrypt.hash(req.body.changePassword, 8, function(err, hashedPassword) {
+exports.changePasswordHandler = function (req, res) {
+    bcrypt.hash(req.body.changePassword, 8, function (err, hashedPassword) {
         if (err) {
             console.log('failed to hash password:');
             console.log(err);
-            
-            sendBackJSON({"error": "server error"}, res);
+
+            sendBackJSON({
+                "error": "server error"
+            }, res);
             return;
         }
-        
+
         var user_id = req.body.user_id;
-        db.query("UPDATE LOGIN_CREDENTIALS SET password = '"+ hashedPassword +"' WHERE user_id=" + user_id).spread(function(results, metadata){
+        db.query("UPDATE LOGIN_CREDENTIALS SET password = '" + hashedPassword + "' WHERE user_id=" + user_id).spread(function (results, metadata) {
             var returnJSON = {
-                "success": "Change Password Success"
+                "status": "success",
+                "message": "Change Password Success"
             }
             sendBackJSON(returnJSON, res);
-        }).catch(function(err){
+        }).catch(function (err) {
             console.log("Err in change password");
             var returnJSON = {
-                "error": "Err in change password"
+                "status": "error",
+                "message": "Err in change password"
             }
             sendBackJSON(returnJSON, res);
         });
     });
 };
-exports.unenrollHandler = function (req, res){
+exports.unenrollHandler = function (req, res) {
     var user_id = req.param('user_id');
     var class_id = req.param('class_id');
-    db.query("DELETE FROM ENROLMENT WHERE user_id=" + user_id + " AND class_id =" + class_id).spread(function(results, metadata){
+    db.query("DELETE FROM ENROLMENT WHERE user_id=" + user_id + " AND class_id =" + class_id).spread(function (results, metadata) {
         var returnJSON = {
-            "success": "Delete Success"
+            "status": "success",
+            "message": "Delete Success"
         }
         sendBackJSON(returnJSON, res);
-    }).catch(function (err){
+    }).catch(function (err) {
         console.log("Err in delete course");
         var returnJSON = {
-            "error": "Err in delete course"
+            "status": "error",
+            "message": "Err in delete course"
         }
         sendBackJSON(returnJSON, res);
     })
 };
 
-exports.getProfileHandler = function(req, res){
+exports.getProfileHandler = function (req, res) {
     var id = req.param('user_id');
     db.query("SELECT name, profile_picture_path FROM USERS WHERE id = '" + id + "'").spread(function (results, metadata) {
         db.query("SELECT id, class_name FROM ENROLMENT, CLASSES WHERE id=class_id AND user_id =" + id).spread(function (result, meta) {
             console.log(result);
-            
+
             var returnJSON = {
-                "name": results[0].name,
-                "profile_picture_path": results[0].profile_picture_path,
-                "courses": result
+                "status": "success",
+                "data": {
+                    "name": results[0].name,
+                    "profile_picture_path": results[0].profile_picture_path,
+                    "courses": result
+                },
+                "message": "Success for getting profile",
             }
             sendBackJSON(returnJSON, res);
         })
@@ -136,7 +151,8 @@ exports.getProfileHandler = function(req, res){
     }).catch(function (err) {
         console.log("Err in getting user profile");
         var returnJSON = {
-            "error": "Err in getting user profile"
+            "status": "error",
+            "message": "Err in getting user profile"
         }
         sendBackJSON(returnJSON, res);
     });
@@ -146,17 +162,18 @@ exports.signinHandler = function (req, res) {
     var signinUsername = req.body.signinUsername;
     var signinPassword = req.body.signinPassword;
     db.query("SELECT password FROM LOGIN_CREDENTIALS WHERE username = '" + signinUsername + "'").spread(function (results, metadata) {
-        bcrypt.compare(signinPassword, results[0].password, function(err, result) { 
+        bcrypt.compare(signinPassword, results[0].password, function (err, result) {
             if (err || result === false) {
                 console.log("Err in login");
                 var returnJSON = {
-                    "error": "Err in login"
+                    "status": "error",
+                    "message": "Err in login"
                 }
                 sendBackJSON(returnJSON, res);
-            }
-            else {
+            } else {
                 var returnJSON = {
-                    "success": "Login Success"
+                    "status": "success",
+                    "message": "Login Success"
                 }
                 sendBackJSON(returnJSON, res);
             }
@@ -164,51 +181,61 @@ exports.signinHandler = function (req, res) {
     }).catch(function (err) {
         console.log("Err in signin");
         var returnJSON = {
-            "error": "Err in login"
+            "status": "error",
+            "message": "Err in login"
         }
         sendBackJSON(returnJSON, res);
     });
 }
 
 exports.signupHandler = function (req, res) {
-    bcrypt.hash(req.body.signupPassword, 8, function(err, hashedPassword) {
+    bcrypt.hash(req.body.signupPassword, 8, function (err, hashedPassword) {
         if (err) {
             console.log('failed to hash password:');
             console.log(err);
-            
-            sendBackJSON({"error": "server error"}, res);
+
+            sendBackJSON({
+                "error": "server error"
+            }, res);
             return;
         }
-        
+
         console.log("ab")
         console.log(req.body);
-        
+
         db.transaction(function (transaction) {
-    
-            var signupName = req.body.signupName;
-            var signupUsername = req.body.signupUsername;
-            var signupProfilePicture = req.body.signupProfilePicture;
-            return db.query("INSERT INTO USERS (name, profile_picture_path) VALUES ('" + signupName + "','" + signupProfilePicture + "')", {transaction: transaction}).then(function (result) {
-                var metadata = result[1];
-                return loginInsert(transaction, metadata.lastID, signupUsername, hashedPassword, res);
+
+                var signupName = req.body.signupName;
+                var signupUsername = req.body.signupUsername;
+                var signupProfilePicture = req.body.signupProfilePicture;
+                return db.query("INSERT INTO USERS (name, profile_picture_path) VALUES ('" + signupName + "','" + signupProfilePicture + "')", {
+                    transaction: transaction
+                }).then(function (result) {
+                    var metadata = result[1];
+                    return exports.loginInsert(transaction, metadata.lastID, signupUsername, hashedPassword, res);
+                });
+            })
+            .catch(function (err) {
+                console.log("failed to create user:");
+                console.log(err);
+                var returnJSON = {
+                    "status": "error",
+                    "message": "failed to create user"
+                }
+                sendBackJSON(returnJSON, res);
             });
-        })
-        .catch(function(err) {
-            console.log("failed to create user:");
-            console.log(err);
-            
-            sendBackJSON({"error": "failed to create user"}, res);
-        });
     });
 };
 
 exports.loginInsert = function (transaction, id, signupUsername, signupPassword, res) {
-    return db.query("INSERT INTO LOGIN_CREDENTIALS (user_id, username, password) VALUES (" + id + ",'" + signupUsername + "','" + signupPassword + "')", {transaction: transaction})
-    .then(function (result, metadata) {
-        var returnJSON = {
-            "success": "Signup Success"
-        };
-        sendBackJSON(returnJSON, res);
-    });
+    return db.query("INSERT INTO LOGIN_CREDENTIALS (user_id, username, password) VALUES (" + id + ",'" + signupUsername + "','" + signupPassword + "')", {
+            transaction: transaction
+        })
+        .then(function (result, metadata) {
+            var returnJSON = {
+                "status": "success",
+                "message": "Signup Success"
+            };
+            sendBackJSON(returnJSON, res);
+        });
 };
-
