61dbdb6928a13c78927b8d82490d66238c6cc958
sk-csc309 <sajjad.forums@gmail.com>
2016-07-24 23:08:45 -0400

    Recommended courses now appear on the home page.

--- home.js
+++ home.js
@@ -2,38 +2,38 @@ var common = require('./common');
 var recommendations = require('./recommendations');
 
 
-function renderPage(req, res, errorContent, popularCourses, recommendedCourses) {
+function renderPage(req, res, errorContent, popularClasses, recommendedClasses) {
     res.render('home', {
         loggedIn: common.userIsLoggedIn(req),
         errorContent: errorContent,
-        popularCourses: popularCourses,
-        popularWellColumnSize: popularCourses ? 12 / popularCourses.length : 12,
-        recommendedCourses: recommendedCourses,
-        recommendedWellColumnSize: recommendedCourses ? 12 / recommendedCourses.length : 12
+        popularClasses: popularClasses,
+        popularWellColumnSize: popularClasses ? 12 / popularClasses.length : 12,
+        recommendedClasses: recommendedClasses,
+        recommendedWellColumnSize: recommendedClasses ? 12 / recommendedClasses.length : 12
     });
 }
 
 
 exports.render = function(req, res, errorContent) {
     // retrieve the popular courses and if a user is logged in, the recommended courses
-    recommendations.popularCourses(function(err, popularCourses) {
+    recommendations.popularClasses(function(err, popularClasses) {
         if (err) {
             console.log(err);
             renderPage(req, res, '<p><strong>Oh no!</strong> We couldn\'t load the popular courses.</p>');
         }
         else if (common.userIsLoggedIn(req)) {
-            recommendations.recommendedCourses(common.getLoggedInUserId(req), function(err, recommendedCourses) {
+            recommendations.recommendedClasses(common.getLoggedInUserId(req), function(err, recommendedClasses) {
                 if (err) {
                     console.log(err);
-                    renderPage(req, res, '<p><strong>Oh no!</strong> We couldn\'t load recommended courses for you.</p>', popularCourses);
+                    renderPage(req, res, '<p><strong>Oh no!</strong> We couldn\'t load recommended classes for you.</p>', popularClasses);
                 }
                 else {
-                    renderPage(req, res, null, popularCourses, recommendedCourses);
+                    renderPage(req, res, null, popularClasses, recommendedClasses);
                 }
             });
         }
         else {
-            renderPage(req, res, null, popularCourses);
+            renderPage(req, res, null, popularClasses);
         }
     });
 }
--- recommendations.js
+++ recommendations.js
@@ -3,31 +3,28 @@ var common = require('./common');
 var db = common.db;
 var sendBackJSON = common.sendBackJSON;
 
-/* Calls the callback with a list of up to 3 recommended courses for the logged in user.
+/* Calls the callback with a list of up to 3 recommended clases for the logged in user.
  *
  * Recommended courses are determined by finding all of a user's classmates and
  * finding the classes which those classmates are taking, returning them in
  * order based on how many fellow classmates are also taking the other courses.
  */
-exports.recommendedCourses = function(userId, callback) {
+exports.recommendedClasses = function(userId, callback) {
     var queryString =
-        'SELECT E.class_id, U.name, ' +
-            'count(*) as user_count, C.class_name ' +
-        'FROM ENROLMENT E ' +
-        'INNER JOIN Classes C ' +
-            'ON C.id = E.class_id ' +
-        'INNER JOIN USERS U ' +
-            'ON U.id = C.instructor ' +
-        'WHERE E.user_id IN ( ' +
-        	'SELECT DISTINCT E2.user_id ' +
-        	'FROM ENROLMENT E2 ' +
-        	'WHERE E2.class_id in ( ' +
-        		'SELECT DISTINCT E1.class_id ' +
-        		'FROM ENROLMENT E1 ' +
-        		'WHERE E1.user_id = $1 ' +
-        	') ' +
+        'SELECT C.id as class_id, C.class_name, count(*) as user_count ' +
+        'FROM CLASSES C ' +
+        'INNER JOIN ENROLMENT E ' +
+            'ON E.class_id = C.id ' +
+        'INNER JOIN FOLLOWINGS F ' +
+            'ON F.followee = E.user_id ' +
+        // exclude classes the user is already in
+        'WHERE C.id NOT IN ( ' +
+            'SELECT E1.class_id ' +
+            'FROM ENROLMENT E1 ' +
+            'WHERE E1.user_id = $1 ' +
         ') ' +
-        'GROUP BY E.class_id ' +
+        'AND F.follower = $1 ' +
+        'GROUP BY C.id ' +
         'ORDER BY user_count DESC ' +
         'LIMIT 3 '
     ;
@@ -42,8 +39,8 @@ exports.recommendedCourses = function(userId, callback) {
 }
 
 
-/* Calls the callback with up to three of the most popular courses. */
-exports.popularCourses = function(callback) {
+/* Calls the callback with up to three of the most popular classes. */
+exports.popularClasses = function(callback) {
     var queryString =
         'SELECT E.class_id, U.name as instructor, ' +
             'count(*) AS user_count, C.class_name ' +
