4838c54c230c1be85b026e81537fb55c2848863b
Willis Chien <willis.chien@mail.utoronto.ca>
2016-07-26 23:23:39 -0400

    change password done

--- public/js/index.js
+++ public/js/index.js
@@ -126,6 +126,7 @@ var loginPage = function () {
         type: 'text',
         placeholder: 'Username',
         required: 'required',
+        pattern: '^[a-zA-Z0-9]{8,20}',
         class: 'standard-red-input'
     });
 
@@ -137,6 +138,7 @@ var loginPage = function () {
         type: 'password',
         placeholder: 'Password',
         required: 'required',
+        pattern: '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&\-])[A-Za-z\d$@$!%*#?&\-]{8,20}$',
         class: 'standard-red-input'
     });
 
@@ -186,9 +188,10 @@ var loginPage = function () {
         hidePopup();
         window.location = "./profile";
     });
-
+    
+    
     $input.appendTo($form);
-
+    
     $form.appendTo($container);
     $container.appendTo($middle);
     $middle.appendTo($outer);
@@ -247,6 +250,7 @@ var signupPage = function () {
         type: 'text',
         placeholder: 'Username',
         required: 'required',
+        pattern: '^[a-zA-Z0-9]{8,20}$',
         class: 'standard-input'
     });
 
@@ -258,6 +262,7 @@ var signupPage = function () {
         type: 'password',
         placeholder: 'Password',
         required: 'required',
+        pattern: '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&\-])[A-Za-z\d$@$!%*#?&\-]{8,20}$',
         class: 'standard-input'
     });
 
@@ -269,6 +274,7 @@ var signupPage = function () {
         type: 'password',
         placeholder: 'Password Comfirmation',
         required: 'required',
+        pattern: '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&\-])[A-Za-z\d$@$!%*#?&\-]{8,20}$',
         class: 'standard-input'
     });
 
@@ -339,8 +345,6 @@ var signupPage = function () {
     
     $form.submit(function(e){
         e.preventDefault();
-        console.log($('#name').val());
-        console.log($('#userPassword').val());
         var body = {
             "signupName": $('#signupName').val(),
             "signupUsername": $('#signupUsername').val(),
