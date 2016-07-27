c9fd9c573a8b887eded352becb3b7e0a80a4259d
sk-csc309 <sajjad.forums@gmail.com>
2016-07-26 21:58:59 -0400

    Added recent notifications to the home page.

--- home.js
+++ home.js
@@ -1,13 +1,15 @@
 var common = require('./common');
 var recommendations = require('./recommendations');
+var notifications = require('./notifications');
 
 
-function renderPage(req, res, errorContent, popularClasses, recommendedClasses) {
+function renderPage(req, res, errorContent, popularClasses, recommendedClasses, notifications) {
     res.render('home', {
         loggedIn: common.userIsLoggedIn(req),
         errorContent: errorContent,
         popularClasses: popularClasses,
-        recommendedClasses: recommendedClasses
+        recommendedClasses: recommendedClasses,
+        notifications: notifications
     });
 }
 
@@ -20,13 +22,33 @@ exports.render = function(req, res, errorContent) {
             renderPage(req, res, '<p><strong>Oh no!</strong> We couldn\'t load the popular courses.</p>');
         }
         else if (common.userIsLoggedIn(req)) {
-            recommendations.recommendedClasses(common.getLoggedInUserId(req), 6, function(err, recommendedClasses) {
+            var userId = common.getLoggedInUserId(req);
+            recommendations.recommendedClasses(userId, 6, function(err, recommendedClasses) {
                 if (err) {
-                    console.log(err);
-                    renderPage(req, res, '<p><strong>Oh no!</strong> We couldn\'t load recommended classes for you.</p>', popularClasses);
+                    // even if we fail, try to load notifications
+                    notifications.recentNotifications(userId, 10, function(err, notifications) {
+                        if (err) {
+                            console.log(err);
+                            renderPage(req, res, '<p><strong>Oh no!</strong> We couldn\'t load notifications or recommended classes for you.</p>', popularClasses);
+                        }
+                        else {
+                            console.log(err);
+                            renderPage(req, res, '<p><strong>Oh no!</strong> We couldn\'t load recommended classes for you.</p>', popularClasses);
+                        }
+                    });
                 }
                 else {
-                    renderPage(req, res, null, popularClasses, recommendedClasses);
+                    // finally, load the notification feed
+                    notifications.recentNotifications(userId, 10, function(err, notifications) {
+                        if (err) {
+                            console.log(err);
+                            var errorContent = '<p><strong>Doh!</strong> We weren\'t able to load your notification feed.';
+                            renderPage(req, res, errorContent, popularClasses, recommendedClasses, null);
+                        }
+                        else {
+                            renderPage(req, res, null, popularClasses, recommendedClasses, notifications);
+                        }
+                    });
                 }
             });
         }
--- notifications.js
+++ notifications.js
@@ -28,29 +28,6 @@ function notifyOfClassEnrolment(userId, classId) {
 }
 
 
-/* Marks all provided notification as seen, provided that they belong to the
- * given user.
- *
- * Returns the result of the callback. Note that the callback is called with
- * an error if one occurred, or null otherwise.
- */
-function markAsSeen(notificationIds, userId) {
-    var query =
-        'UPDATE NOTIFICATIONS ' +
-        'SET seen = 1 ' +
-        'WHERE notification_id IN [$1] AND user_id = $2 '
-    ;
-    
-    db.query(query, { bind: [notificationIds, userId] })
-    .spread(function(results, metadata) {
-        return callback(null);
-    })
-    .catch(function(err) {
-        return callback(err);
-    });
-}
-
-
 /* Fetches an array of most recent notifications, up to the provided limit,
  * ordered from most recent to least recent. If the limit is negative,
  * fetches up to ten notifications.
@@ -60,18 +37,20 @@ function markAsSeen(notificationIds, userId) {
  */
 function recentNotifications(userId, limit, callback) {
     var query =
-        'SELECT N.id, N.seen, N.created_timestamp, ' +
-            'U.name, ' +
-            'C.class_id, C.class_name, I.name ' +
-        'FROM NOTIFICATIONS N' +
+        'SELECT N.id AS notification_id, N.created_timestamp AS timestamp, ' +
+            'U.name AS followee_name, ' +
+            'C.id AS class_id, C.class_name, I.name ' +
+        'FROM NOTIFICATIONS N ' +
         // the folowee's information
         'INNER JOIN USERS U ' +
-            'ON U.id = N.followee_id '
+            'ON U.id = N.followee_id ' +
         // instructor's information
         'INNER JOIN USERS I ' +
             'ON I.id = C.instructor ' +
-        'WHERE user_id = $1' +
-        'ORDER BY created_timestamp DESC ' +
+        'INNER JOIN CLASSES C ' +
+            'ON C.id = N.class_id ' +
+        'WHERE user_id = $1 ' +
+        'ORDER BY N.created_timestamp DESC ' +
         'LIMIT $2'
     ;
     
@@ -90,5 +69,4 @@ function recentNotifications(userId, limit, callback) {
 
 
 exports.notifyOfClassEnrolment = notifyOfClassEnrolment;
-exports.markAsSeen = markAsSeen;
 exports.recentNotifications = recentNotifications;
