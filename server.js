e35ecd4a161a6fa9ce0a26c9dfd9d3aa1a9a80a4
Willis Chien <willis.chien@mail.utoronto.ca>
2016-07-27 18:50:04 -0400

    try upload does not work

--- server.js
+++ server.js
@@ -133,6 +133,9 @@ var upload = multer({
     dest: __dirname + '/public/img/'
 }).single('courseBanner');
 app.post('/createcourse', checkAuthentication, function (req, res, next) {
+    console.log(req);
+    console.log("FILE" + req.file);
+    console.log("FILES" + req.files);
     upload(req, res, function (err) {
         res.courseBannerErr = '';
         res.courseTitleErr = '';
