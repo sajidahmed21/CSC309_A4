716547ef817f9c6c4ad824fd37f747d53c8bb056
sk-csc309 <sajjad.forums@gmail.com>
2016-07-28 22:32:12 -0400

    Consolidated footer on all pages.

--- public/js/popups.js
+++ public/js/popups.js
@@ -1,84 +1,89 @@
 $(document).ready(function () {
-    //initialize();
     $('#signup_btn').click(function () {
         signupPage();
     });
+    
     $('#userlogin_btn').click(function () {
         loginPage();
     });
+    
     $('#reference').click(function () {
         referencePopup();
     });
+    
+    $('.cancel_icon').click(function(){
+        hidePopup();
+    });
 });
 
-var referencePopup = function () {
+var referencePopup = function(){
     var $popup = $('#popup');
     $popup.empty();
-
+    
     $outer = $('<div/>', {
-        id: 'reference_form'
+       id:  'reference_form'
     });
-
+    
     $middle = $('<div/>', {
-        class: 'col-md-6'
+       class:  'col-md-6'
     });
-
+    
     $container = $('<div/>', {
-        class: 'standard-ref-container'
+       class:  'standard-ref-container'
     });
-
+    
     $cancelwrapper = $('<div/>');
-
+    
     $cancelicon = $('<span/>', {
-        class: 'glyphicon glyphicon-remove cancel_icon'
+       class:  'glyphicon glyphicon-remove cancel_icon'
     });
-    $cancelicon.click(function () {
+    $cancelicon.click(function(){
         hidePopup();
     });
     $cancelwrapper.append($cancelicon);
-
+    
     $container.append($cancelwrapper);
-
+    
     $form = $('<form/>');
-
-    $title = $('<h1/>', {
-        class: 'standard-title'
+    
+    $title = $('<h1/>',{
+        class : 'standard-title'
     });
     $title.text('References').appendTo($form);
-
+    
     $ul = $('<ul/>');
-
+    
     $ul.css('list-style-type', 'none');
     $ul.css('margin', '0');
     $ul.css('padding', '0');
     $ul.css('text-align', 'center');
-
-    $li = $('<li/>', {
+    
+    $li = $('<li/>',{
         class: 'standard_ref_list'
     });
-
+    
     $li.text('Youtube. (Company). (2005). maxresdefault. [Digital image]. Retrieved from https://i.ytimg.com/vi/MWOdIIGOHGU/maxresdefault.jpg');
-
+            
     $li.appendTo($ul);
-
+    
     $li = $('<li/>', {
         class: 'standard_ref_list'
     });
-
+    
     $li.text('Youtube. (Company). (2005). maxresdefault. [Digital image]. Retrieved from https://i.ytimg.com/vi/LrGPkzy6mFo/maxresdefault.jpg');
-
+            
     $li.appendTo($ul);
-
-    $li = $('<li/>', {
+    
+    $li = $('<li/>',{
         class: 'standard_ref_list'
     });
-
+    
     $li.text('boredpanda. (Website). origami-crane-paper-art-fb. [Digital image]. Retrieved from http://static.boredpanda.com/blog/wp-content/uploads/2015/10/origami-crane-paper-art-fb.jpg');
-
+            
     $li.appendTo($ul);
-
+    
     $ul.appendTo($form);
-
+            
     $form.appendTo($container);
     $container.appendTo($middle);
     $middle.appendTo($outer);
