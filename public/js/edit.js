964a1026b977cb7aadc1e5e1ddb9bd0ca8e0fc4b
jubile <erfahabib@gmail.com>
2016-07-30 18:08:29 -0400

    fixed small error

--- public/js/edit.js
+++ public/js/edit.js
@@ -84,7 +84,7 @@ $(document).ready(function () {
                 console.log(input);
                 
                  var insertthis = $("<p class=standard-font></p>").text(input);
-                $("#input-requirements").replaceWith("insertthis");
+                $("#input-requirements").replaceWith(insertthis);
                 var btn = create_edit_btn().attr('id', "edit-requirements");
                 $("#save-requirements").replaceWith(btn);
             },
