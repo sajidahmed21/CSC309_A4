abf921361ab5ba021c599b5c273ccfb5b9e8fc26
sk-csc309 <sajjad.forums@gmail.com>
2016-07-29 20:45:40 -0400

    Reverted to created popups in javascript.

--- public/js/popups.js
+++ public/js/popups.js
@@ -1,32 +1,15 @@
 /* Sets all onclick events for the signup, login, and reference popups. */
 $(document).ready(function () {
-    // hide popups on load
-    hidePopups();
-    
     // set events to show popups
     $('#signup_btn').click(showSignup);
     $('#userlogin_btn').click(showSignin);
     $('#reference').click(showReferences);
-    
-    // attach an onclick onto all cancel icons
-    $('div#popups .cancel_icon').click(hidePopups);
-    
-    // redirection for Google
-    $('button#google-login').click(redirectToGoogleAuthentication);
-    
-    // hide the signup error box
-    $('#signup-error-message').hide();
-    
-    // attach form submition events
-    $('form#signup-popup').submit(onSignup);
 });
 
 
-/* Hides all popups. */
-function hidePopups() {
-    $('form#signin-popup').hide();
-    $('form#signup-popup').hide();
-    $('div#references-popup').hide();
+/* Destroys any existing popup. */
+function hidePopup() {
+    $('#popup').empty();
 };
 
 
@@ -36,37 +19,298 @@ function redirectToGoogleAuthentication() {
 }
 
 
-/* Hides all popups and then displays the references popup. */
+/* Destroys any existing popup and then displays the references popup. */
 var showReferences = function(){
-    hidePopups();
-    $('div#references-popup').show();
+    var $popup = $('#popup');
+    $popup.empty();
+    
+    $outer = $('<div/>', {
+       id:  'reference_form'
+    });
+    
+    $middle = $('<div/>', {
+       class:  'col-md-6'
+    });
+    
+    $container = $('<div/>', {
+       class:  'standard-ref-container'
+    });
+    
+    $cancelwrapper = $('<div/>');
+    
+    $cancelicon = $('<span/>', {
+       class:  'glyphicon glyphicon-remove cancel_icon'
+    });
+    $cancelicon.click(function(){
+        hidePopup();
+    });
+    $cancelwrapper.append($cancelicon);
+    
+    $container.append($cancelwrapper);
+    
+    $form = $('<form/>');
+    
+    $title = $('<h1/>',{
+        class : 'standard-title'
+    });
+    $title.text('References').appendTo($form);
+    
+    $ul = $('<ul/>');
+    
+    $ul.css('list-style-type', 'none');
+    $ul.css('margin', '0');
+    $ul.css('padding', '0');
+    $ul.css('text-align', 'center');
+    
+    $li = $('<li/>',{
+        class: 'standard_ref_list'
+    });
+    
+    $li.text('Youtube. (Company). (2005). maxresdefault. [Digital image]. Retrieved from https://i.ytimg.com/vi/MWOdIIGOHGU/maxresdefault.jpg');
+            
+    $li.appendTo($ul);
+    
+    $li = $('<li/>', {
+        class: 'standard_ref_list'
+    });
+    
+    $li.text('Youtube. (Company). (2005). maxresdefault. [Digital image]. Retrieved from https://i.ytimg.com/vi/LrGPkzy6mFo/maxresdefault.jpg');
+            
+    $li.appendTo($ul);
+    
+    $li = $('<li/>',{
+        class: 'standard_ref_list'
+    });
+    
+    $li.text('boredpanda. (Website). origami-crane-paper-art-fb. [Digital image]. Retrieved from http://static.boredpanda.com/blog/wp-content/uploads/2015/10/origami-crane-paper-art-fb.jpg');
+            
+    $li.appendTo($ul);
+    
+    $ul.appendTo($form);
+            
+    $form.appendTo($container);
+    $container.appendTo($middle);
+    $middle.appendTo($outer);
+    $outer.appendTo($popup);
 };
 
 
-/* Hides all popups and then displays the signin popup. */
+/* Destroys any existing popup and then displays the signin popup. */
 var showSignin = function () {
-    hidePopups();
-    $('form#signin-popup').show();
+    var $popup = $('#popup');
+    $popup.empty();
+
+    $outer = $('<div/>', {
+        id: 'userlogin_form'
+    });
+
+    $middle = $('<div/>', {
+        class: 'col-md-6'
+    });
+
+    $container = $('<div/>', {
+        class: 'standard-container'
+    });
+
+    $cancelwrapper = $('<div/>');
+
+    $cancelicon = $('<span/>', {
+        class: 'glyphicon glyphicon-remove cancel_icon'
+    });
+    $cancelicon.click(function () {
+        hidePopup();
+    });
+    $cancelwrapper.append($cancelicon);
+
+    $container.append($cancelwrapper);
+
+    $form = $('<form method="post" action="/user/signin"/>');
+
+    $title = $('<h1/>', {
+        class: 'standard-title'
+    });
+    $title.text('Login').appendTo($form);
+
+    $input = $('<input/>', {
+        id: 'signinUsername',
+        name: 'signinUsername',
+        type: 'text',
+        placeholder: 'Username',
+        required: 'required',
+        pattern: '^[a-zA-Z0-9]{8,20}$',
+        title: 'Username must be between 8 and 20 characters, and must only consist of letters and numbers.',
+        class: 'standard-red-input'
+    });
+
+    $input.appendTo($form);
+
+    $input = $('<input/>', {
+        id: 'signinPassword',
+        name: 'signinPassword',
+        type: 'password',
+        placeholder: 'Password',
+        required: 'required',
+        pattern: '^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&\\-])[A-Za-z\\d$@$!%*#?&\\-]{8,20}$',
+        title: 'Password must contain at least 1 letter, 1 number, 1 special character, ' +
+            ' and must be between 8 and 20 characters long. Special characters include $@!%*#?&-',
+        class: 'standard-red-input'
+    });
+
+    $input.appendTo($form);
+
+    $input = $('<button/>', {
+        type: 'submit',
+        text: 'Login',
+        class: 'btn btn-danger standard-red-button block_btn',
+    });
+
+    $input.appendTo($form);
+
+    $input = $('<input/>', {
+        type: 'button',
+        value: 'Google Login',
+        class: 'btn btn-danger standard-red-button block_btn'
+    });
+
+    $input.css('margin-top', '10px');
+
+    $input.click(redirectToGoogleAuthentication);
+
+
+    $input.appendTo($form);
+
+    $form.appendTo($container);
+    $container.appendTo($middle);
+    $middle.appendTo($outer);
+    $outer.appendTo($popup);
     
     // throw the focus onto the first field
     $('input#signinUsername').focus();
 };
 
 
-/* Hides all popups and then displays the signup popup. */
+/* Destroys any existing popup and then displays the signup popup. */
 var showSignup = function () {
-    hidePopups();
-    $('form#signup-popup').show();
+    var $popup = $('#popup');
+    $popup.empty();
+
+    $outer = $('<div/>', {
+        id: 'signup_form'
+    });
+
+    $middle = $('<div/>', {
+        class: 'col-md-6'
+    });
+
+    $container = $('<div/>', {
+        class: 'standard-container'
+    });
+
+    $cancelwrapper = $('<div/>');
+
+    $cancelicon = $('<span/>', {
+        class: 'glyphicon glyphicon-remove cancel_icon'
+    });
+    $cancelicon.click(function () {
+        hidePopup();
+    });
+    $cancelwrapper.append($cancelicon);
+
+    $container.append($cancelwrapper);
+
+    $form = $('<form method="post" action="/user/signup"/>');
+
+    $title = $('<h1/>', {
+        class: 'standard-title'
+    });
+    $title.text('Sign Up').appendTo($form);
+
+    $input = $('<input/>', {
+        id: 'signupName',
+        name: 'signupName',
+        type: 'text',
+        placeholder: 'Your Name',
+        required: 'required',
+        class: 'standard-input'
+    });
+
+    $input.appendTo($form);
+
+    $input = $('<input/>', {
+        id: 'signupUsername',
+        name: 'signupUsername',
+        type: 'text',
+        placeholder: 'Username',
+        required: 'required',
+        pattern: '^[a-zA-Z0-9]{8,20}$',
+        title: 'Username must be between 8 and 20 characters, and must only consist of letters and numbers.',
+        class: 'standard-input'
+    });
+
+    $input.appendTo($form);
+
+    $input = $('<input/>', {
+        id: 'signupPassword',
+        name: 'signupPassword',
+        type: 'password',
+        placeholder: 'Password',
+        required: 'required',
+        pattern: '^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&\\-])[A-Za-z\\d$@$!%*#?&\\-]{8,20}$',
+        title: 'Password must contain at least 1 letter, 1 number, 1 special character, ' +
+            ' and must be between 8 and 20 characters long. Special characters include $@!%*#?&-',
+        class: 'standard-input'
+    });
+
+    $input.appendTo($form);
+
+    $input = $('<input/>', {
+        id: 'userPasswordConfirm',
+        name: 'userPasswordConfirm',
+        type: 'password',
+        placeholder: 'Password Comfirmation',
+        required: 'required',
+        pattern: '^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&\\-])[A-Za-z\\d$@$!%*#?&\\-]{8,20}$',
+        title: 'Password must contain at least 1 letter, 1 number, 1 special character, ' +
+            ' and must be between 8 and 20 characters long. Special characters include $@!%*#?&-',
+        class: 'standard-input'
+    });
+
+    $input.appendTo($form);
+
+    $div = $('<div/>', {
+        id: 'signup-error-message',
+        class: 'invis'
+    });
+
+    $div.text('Your passwords do not match')
+    $div.appendTo($form);
+
+    $input = $('<button/>', {
+        type: 'submit',
+        text: 'Join Now!',
+        class: 'standard-blue-button block_btn',
+    });
+
+    $input.appendTo($form);
+
+    $form.submit(validateSignup);
+
+    $form.appendTo($container);
+    $container.appendTo($middle);
+    $middle.appendTo($outer);
+    $outer.appendTo($popup);
     
     // throw the focus onto the first field
     $('input#signupName').focus();
 };
 
 
-/* Handles when the user submits a signup request by validating input
- * and making an AJAX call if everything is valid.
+/* Called when the user submits a signup request by validating input and adds
+ * any error message to the error dialogue area.
+ *
+ * Returns true if the validation was successful, or false otherwise.
  */
-function onSignup(form) {
+function validateSignup(form) {
     var $error = $('#signup-error-message');
     
     // hide any existing error
